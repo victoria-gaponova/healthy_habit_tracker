2e9dc2e6d106aa0e9d611fa0f341dc7b298d320a
Vika <victoria.gaponava@gmail.com>
2023-11-22 23:35:46 +0300

    add validators for habit

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import validate_related_habit_and_reward, validate_time_to_complete_habit, \
+    validate_pleasant_habit, validate_periodicity
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -8,3 +10,11 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, data):
+        """Валидаторы для модели Habit."""
+        validate_related_habit_and_reward(data)
+        validate_time_to_complete_habit(data)
+        validate_pleasant_habit(data)
+        validate_periodicity(data)
+        return data
---
+++ habits/validators.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+
+def validate_related_habit_and_reward(value):
+    """Валидатор: Исключить одновременный выбор связанной привычки и указания вознаграждения."""
+    if value.get('related_habit') and value.get('reward'):
+        raise serializers.ValidationError("You can't choose both a related habit and a reward.")
+
+
+def validate_time_to_complete_habit(value):
+    """Валидатор: Время выполнения должно быть не больше 120 секунд."""
+    if value.get('time_to_complete_habit'):
+        raise serializers.ValidationError("Time to complete habit should not exceed 120 seconds.")
+
+
+def validate_pleasant_habit(value):
+    """Валидатор: У приятной привычки не может быть вознаграждения или связанной привычки."""
+    if value.get('is_pleasant_habit') and value.get('reward') or value.get('related_habit'):
+        serializers.ValidationError("A pleasant habit should not have a reward or a related habit.")
+
+
+def validate_periodicity(value):
+    """Валидатор: Нельзя выполнять привычку реже, чем 1 раз в 7 дней."""
+    if value.get('periodicity') not in ['daily', 'weekly']:
+        serializers.ValidationError("The minimum periodicity is once in 7 days for non-daily habits.")
